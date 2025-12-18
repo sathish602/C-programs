f7d932aefcf6013091b32d315e449cb2ca8c3907
sathish602 <bandasathish602@gmail.com>
2025-12-18 22:16:10 +0530

    Refactor Even or Odd program for clarity
    Removed unnecessary comments and whitespace.

--- Even or Odd.c
+++ Even or Odd.c
@@ -1,17 +1,10 @@
 // C Program to Check Even or Odd Using Modulo Operator
 #include <stdio.h>
-
 void checkOddEven(int N) {
-  
-    // Find the remainder
     int r = N % 2;
-
-    // Condition for even
     if (r == 0)  {
         printf("Even");
     }
-  
-    // Condition for odd number
     else  {
         printf("Odd");
     }
