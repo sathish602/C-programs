ff207e312269f46df360de00ef366ea553b970e4
sathish602 <bandasathish602@gmail.com>
2025-12-22 14:14:01 +0530

    Create array By 90 degree in clockwise direction.c

---
+++ array By 90 degree in clockwise direction.c
@@ -0,0 +1,45 @@
+// C Program to rotate the array By 90 degree in clockwise direction
+#include <stdio.h>
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+int n = 4;
+    int arr[4][4] = { { 1, 2, 3, 4 },
+                      { 5, 6, 7, 8 },
+                      { 9, 10, 11, 12 },
+                      { 13, 14, 15, 16 } };
+
+    // Print Orignal Matrix
+    printf("Orignal Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Rotate the matrix about the main diagonal
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++)
+            swap(&arr[i][j], &arr[j][i]);
+    }
+
+    // Rotate the matrix about middle column
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            swap(&arr[i][j], &arr[i][n - j - 1]);
+        }
+    }
+ // Print the rotated matrix
+    printf("Matrix after rotation: \n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
