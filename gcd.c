96b491ba8fd2560c5f7990a10bec76f3c9afd37b
sathish602 <bandasathish602@gmail.com>
2025-12-27 23:44:18 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,23 +1,17 @@
-// C program to find GCD of two numbers
 #include <stdio.h>
-int gcd(int a, int b)
-{
-    // Everything divides 0
-    if (a == 0)
-        return b;
-    if (b == 0)
-        return a;
-// base case
-    if (a == b)
-        return a;
-// a is greater
-    if (a > b)
-        return gcd(a - b, b);
-    return gcd(a, b - a);
-}
-int main()
-{
-    int a = 192, b = 36;
-    printf("GCD of %d and %d is %d ", a, b, gcd(a, b));
+
+int main() {
+    int a, b, i, gcd;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    for (i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+
+    printf("GCD = %d", gcd);
+
     return 0;
 }
